d4612d812963ddf0670ee5690f2e15ef54c727d4
Vitalii Pshychenko <pshychenko94@gmail.com>
2019-08-16 23:16:39 +0300

    Updates

---
+++ precache-manifest.9b2c54281001d74f01e78879bfa6afb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867315216aee8443cd4f0376be141039",
+    "url": "/react-strore/index.html"
+  },
+  {
+    "revision": "40617dfb454ed8811c23",
+    "url": "/react-strore/static/css/main.fd9339b7.chunk.css"
+  },
+  {
+    "revision": "cd68d28655d751623dac",
+    "url": "/react-strore/static/js/2.deb7ce69.chunk.js"
+  },
+  {
+    "revision": "40617dfb454ed8811c23",
+    "url": "/react-strore/static/js/main.cb983cff.chunk.js"
+  },
+  {
+    "revision": "19d7bb77eeaeab622c62",
+    "url": "/react-strore/static/js/runtime~main.9fe2f8df.js"
+  }
+]);
